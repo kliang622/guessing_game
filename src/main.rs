71d3cafedef425b79b6269decb7e771c434eab22
Kanin Liang <kanin.liang@optum.com>
2021-08-03 12:55:41 -0700

    added number rand function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
 use  std::io; //standard input output
+use std::cmp::Ordering; 
+use rand::Rng; //gets random number in certain range
+
 
 fn main() {
-    println!("Hello, world!");
-    println! ("Please eneter guess between 1-100"); 
-    //stores user values
-    let mut guess = String::new(); 
+    println!("Hello, guess the number!");
+    //values are inclusive on lower bound and exclusive on upper bound
+    let guessNumber = rand::thread_rng().gen_range(1..101); 
+    
+    loop{
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line" );
-        //{} is place hodler for number guessed
-        println! ("Your guess: {}", guess);
+        println! ("Please eneter guess between 1-100"); 
+        //stores user values
+        let mut guess = String::new(); 
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line" );
+
+            let guess: u32 = match guess.trim().parse()
+            {
+                Ok(num) => num, 
+                Err(_)=> continue, 
+            };
+            //{} is place hodler for number guessed
+            println! ("Your guess: {}", guess);
+        //match is made of arms which consists of patter and code that is run when something meets the requeiremnts
+        match guess.cmp(&guessNumber){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You got it!!");
+                break; //ends
+        }
+    }
+}
 }
