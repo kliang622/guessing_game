14576011ca4a4d636611d976e0228c0a3532dd55
Kanin Liang <kanin.liang@optum.com>
2021-08-03 11:01:23 -0700

    get user input and prints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use  std::io; //standard input output
+
 fn main() {
     println!("Hello, world!");
+    println! ("Please eneter guess between 1-100"); 
+    //stores user values
+    let mut guess = String::new(); 
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line" );
+        //{} is place hodler for number guessed
+        println! ("Your guess: {}", guess);
 }
